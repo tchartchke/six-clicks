0a34786df5b7a669b9553742db24024a8dd8ff64
tchartchke <chartai.kl@gmail.com>
2021-07-22 18:08:48 -0500

    white spaces and small fixes

--- src/components/PreviousPlays.js
+++ src/components/PreviousPlays.js
@@ -4,7 +4,7 @@ const PreviousPlays = (props) => (
   <div>
     <h1>Previously Played Missions</h1>
     {props.plays.map(play => 
-      <div>
+      <div key={play.id}>
         {play.name} - {play.start} to {play.end} : {play.clicks}
       </div>
     )}
