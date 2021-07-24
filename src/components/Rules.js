44bad33c6ccfef613f3834d553d83bf00fc3dc9b
tchartchke <chartai.kl@gmail.com>
2021-07-24 10:27:17 -0500

    css to differentiate links

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -10,11 +10,11 @@ const Rules = () => {
           Starting from one <a href='https://en.wikipedia.org/wiki/Main_Page'>Wikipedia</a> article, use the links on the page to travel from one article to another designated article in the least number of clicks. Articles can be explicited selected or randomized.
         </p>
         <p>
-        The game originated in Liverpool, England sometime around 2010 and was the brain child of - Stephen Scragg and Theo Di Caprio, both are now renowned as the “godfathers” of the <a href='https://en.wikipedia.org/wiki/Wikipedia:Wikirace'>Wiki Race game</a> and an annual event is held in Coruna, Spain to celebrate them.
+        The <a href="asdfasdfasdf">game originated in Liverpool, England sometime around</a>2010 and was the brain child of - Stephen Scragg and Theo Di Caprio, both are now renowned as the “godfathers” of the <a href='https://en.wikipedia.org/wiki/Wikipedia:Wikirace'>Wiki Race game</a> and an annual event is held in Coruna, Spain to celebrate them.
         </p>
         <h2>Setup</h2>
         <ol>
-          <li>Pick a start and end point for each <i>Mission</i>. Use specific titles that correspond to the Wikipedia title</li>
+          <li>Pick a start and end point for each <i>Mission</i>. <a href="testtest">Use specific titles that correspond to the Wikipedia title</a></li>
           <li>Check the randomizer checkmark by each end point to select a random value</li>
           <li>Select <i>Validate Mission</i> to verify the page exists on Wikipedia as an article. The <i>Begin Mission</i> button will appear if both end points are valid selections</li>
           <li>Click through the pages to reach the designated</li>
